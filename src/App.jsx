cbf960ed8e6c700bff5eb0250d90e0440b773772
Zual Rana <freelancerzual@gmail.com>
2024-12-24 20:15:20 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Jug from "./components/Jug";
+
+function App() {
+  return (
+    <div className="flex justify-center mt-9">
+      <Jug />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Jug.jsx
@@ -0,0 +1,5 @@
+const Jug = () => {
+  return <></>;
+};
+
+export default Jug;
