e9ba1c6e96643eb03c1e66f10899dc5c69701e7f
Zual Rana <freelancerzual@gmail.com>
2024-12-24 20:16:41 +0600

    created jug and water

--- src/components/Jug.jsx
+++ src/components/Jug.jsx
@@ -1,5 +1,9 @@
 const Jug = () => {
-  return <></>;
+  return (
+    <div className="w-[200px] h-[400px] border-black rounded-sm border-t-0 border-2 relative">
+      <div className="bg-blue-700 w-full h-3/4 bottom-0 absolute"></div>
+    </div>
+  );
 };
 
 export default Jug;
