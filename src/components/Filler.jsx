87e13d46f0f959458e8cf67e720d1f4b12b66a1d
Zual Rana <freelancerzual@gmail.com>
2024-12-25 20:07:07 +0600

    project done

--- src/components/Filler.jsx
+++ src/components/Filler.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Jug from "./Jug";
 
 const Filler = () => {
-  const increaseWater = () => {};
-  const decreaseWater = () => {};
+  let [increaseValue, setIncreaseValue] = useState(0);
+  console.log(increaseValue);
+  const increaseWater = () => {
+    setIncreaseValue((prev) => {
+      if (prev === 0) return 1;
+
+      const filling = prev + prev * 0.4;
+      if (filling > 100) return 100;
+
+      return filling;
+    });
+  };
+  const decreaseWater = () => {
+    setIncreaseValue((prev) => {
+      if (prev < 1) return 0;
+      return prev - prev * 0.2;
+    });
+  };
   return (
     <div>
-      <Jug />
+      <Jug increaseValue={increaseValue} />
       <div className="mt-3 flex justify-center gap-4">
         <button
           onClick={decreaseWater}
--- src/components/Jug.jsx
+++ src/components/Jug.jsx
@@ -1,9 +1,43 @@
-const Jug = () => {
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+
+const Jug = ({ increaseValue }) => {
+  const [isFilling, setIsFilling] = useState(false);
+
+  useEffect(() => {
+    setIsFilling(true);
+
+    const timeoutId = setTimeout(() => {
+      setIsFilling(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [increaseValue]);
+
   return (
     <div className="w-[200px] h-[400px] border-black rounded-sm border-t-0 border-2 relative">
-      <div className="bg-blue-700 w-full h-3/4 bottom-0 absolute"></div>
+      <div
+        className={`bg-blue-700 w-full bottom-0 absolute ${
+          isFilling && "animate-wave"
+        }`}
+        style={{ height: `${increaseValue}%` }}
+      >
+        <p
+          className={`text-center text-white mt-2 ${
+            isFilling && "animate-pulse"
+          } text-xl`}
+        >
+          {Math.round(increaseValue)}%
+        </p>
+      </div>
     </div>
   );
+};
+
+Jug.propTypes = {
+  increaseValue: PropTypes.number.isRequired,
 };
 
 export default Jug;
